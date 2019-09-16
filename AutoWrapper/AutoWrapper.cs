57060e220f5ded4806a155b86aee890245bedcbb
Vincent Maverick S Durano <vmsdurano@live.com.ph>
2019-09-15 23:16:45 -0500

    [FIX] Fix middleware extension name [FET] Add icon

--- AutoWrapper/AutoWrapper.cs
+++ AutoWrapper/AutoWrapper.cs
@@ -5,7 +5,7 @@ namespace AutoWrapper
 {
     public static class AutoWrapperExtension
     {
-        public static IApplicationBuilder UseAutoWrapperWrapper(this IApplicationBuilder builder, AutoWrapperOptions options = default)
+        public static IApplicationBuilder UseApiResponseAndExceptionWrapper(this IApplicationBuilder builder, AutoWrapperOptions options = default)
         {
             options ??= new AutoWrapperOptions();
             return builder.UseMiddleware<AutoWrapperMiddleware>(options);
