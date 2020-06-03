98ac6adf8776ac56df57cc389ff8bc5270aee057
Vincent Maverick S Durano <vmsdurano@gmail.com>
2020-06-02 23:11:15 -0500

    [FET] Added Model Responses for ProblemDetails

---
+++ src/AutoWrapper/Models/ApiProblemDetailsValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AutoWrapper.Models
+{
+    public class ApiProblemDetailsValidationErrorResponse: ProblemDetails
+    {
+        public bool IsError { get; set; }
+        public IEnumerable<ValidationError> ValidationErrors { get; set; }
+    }
+}
