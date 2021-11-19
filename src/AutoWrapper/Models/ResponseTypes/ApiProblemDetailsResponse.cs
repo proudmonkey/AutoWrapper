861d0e5fdc378b67f0296e246a1d99cb0e72112f
Austin Albert <aalbert@tipconsulting.io>
2021-11-19 00:01:35 -0700

    redo last

--- src/AutoWrapper/Models/ResponseTypes/ApiProblemDetailsResponse.cs
+++ src/AutoWrapper/Models/ResponseTypes/ApiProblemDetailsResponse.cs
@@ -5,5 +5,7 @@ namespace AutoWrapper.Models.ResponseTypes
     public class ApiProblemDetailsResponse : ProblemDetails
     {
         public bool IsError { get; set; }
+        public string? ErrorID { get; set; }
+
     }
 }
